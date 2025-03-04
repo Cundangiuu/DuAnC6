ea6c6285f7086807c3a15a633a1c0e4bc2191973
Cundangiuu <nghi2562004@gmail.com>
2025-03-04 19:39:41 +0700

    Mới

---
+++ DuAnBanBanhKeo/Controllers/TaiKhoanController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DuAnBanBanhKeo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaiKhoanController : ControllerBase
+    {
+    }
+}
